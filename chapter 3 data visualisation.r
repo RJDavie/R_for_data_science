478aa3d19f23696f8d7dc9100a9cb3a4e89ed68b
Rob Davie <rob.j.davie@gmail.com>
2019-08-07 11:43:59 +0100

    pre geoms

--- chapter 3 data visualisation.r
+++ chapter 3 data visualisation.r
@@ -7,3 +7,15 @@ library(tidyverse)
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy, shape = class, colour = class))
 
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_wrap(~ cyl)
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(drv ~ cyl)
+
+ggplot(data = mpg, mapping = aes(x = mpg$displ, y = mpg$hwy)) +
+  geom_point() +
+  geom_smooth()
+
